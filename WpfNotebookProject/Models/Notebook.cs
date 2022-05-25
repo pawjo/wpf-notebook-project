905df1989bd68e34b77bec3f1e65ac80ce3aec2b
Paweł Wiszniewski <Pawel.Wiszniewski@billennium.com>
2022-05-25 23:33:22 +0200

    Added basic models

---
+++ WpfNotebookProject/Models/Notebook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfNotebookProject.Models
+{
+    public class Notebook
+    {
+        public string Name { get; set; }
+
+        public string FileName { get; set; }
+
+        public List<Section> Sections { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ WpfNotebookProject/Models/Section.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WpfNotebookProject.Models
+{
+    public class Section
+    {
+        public string Title { get; set; }
+
+        public List<Note> Notes { get; set; }
+    }
+}
